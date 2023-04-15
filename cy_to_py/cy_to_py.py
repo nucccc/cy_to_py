20c923347f21ef6306607d4969cdb4ba64d469bf
Nuc94 <dan.dom.nuc@gmail.com>
2023-04-15 16:35:35 +0200

    added function to check if function declaration has parentheses

--- cy_to_py/cy_to_py.py
+++ cy_to_py/cy_to_py.py
@@ -2,7 +2,7 @@
 in here there shall be the cython to python execution code
 '''
 
-def is_cimport(codeline : str):
+def is_cimport(codeline : str) -> bool:
     '''
     is_cimport returns me if a codeline is a cimport
     '''
@@ -12,15 +12,44 @@ def is_cimport(codeline : str):
         return True
     return False
 
-def is_func_declaration(codeline : str):
+def has_function_call_args(codeline : str) -> bool:
+    '''
+    has_function_call_args returns me if there is a set of arguments,
+    which is a valid condition for a function
+    '''
+    open_par_pos = -1
+    close_par_pos = -1
+    for pos, c in enumerate(codeline):
+        if c == '(':
+            if open_par_pos == -1:
+                open_par_pos = pos
+            else:
+                #in such case this would be the second open parenthesis, and i 
+                #thus return false
+                return False
+        if c == ')':
+            if open_par_pos == -1:
+                #in case i still didn't find an opening parenthesis then i
+                #should return false, since such close parenthesis doesn't
+                #make sense
+                return False
+            elif close_par_pos != -1:
+                close_par_pos = pos
+            else:
+                return False
+    return True
+
+def is_func_declaration(codeline : str) -> bool:
     '''
     is_func_declaration returns me if a line
     '''
-    if (codeline[:5] == 'cpdef' or codeline[:3] == 'def') and codeline[-1] == ':':
-        return True
-    return False
+    if codeline[-1] != ':':
+        return False
+    if codeline[:5] != 'cpdef' and codeline[:3] != 'def' and codeline[:4] != 'cdef':
+        return False
+    return True
 
-def to_rem(codeline : str):
+def to_rem(codeline : str) -> bool:
     '''
     to_rem returns if the codeline needs to be removed
     '''
@@ -28,7 +57,7 @@ def to_rem(codeline : str):
         return True
     return False
 
-def ctp_line(codeline : str):
+def ctp_line(codeline : str) -> str:
     '''
     ctp_line takes in input a code line and converts it back to python from cython
     '''
