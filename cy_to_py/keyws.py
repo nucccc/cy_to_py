27f83d882ab373ad3b7728299284efcfddc6d7ce
Nuc94 <dan.dom.nuc@gmail.com>
2023-04-16 10:16:41 +0200

    added keywords file

---
+++ cy_to_py/keyws.py
@@ -0,0 +1,35 @@
+'''
+in here somehow keywords shall be called and instantiated
+'''
+
+CONTROL_KEYWS = {
+    'for',
+    'while',
+    'if',
+    'elif',
+    'else'
+}
+
+C_TYPES = {
+    'int',
+    'float',
+    'double',
+    'short',
+    'long'
+}
+
+CY_VAR_DEF = 'cdef'
+
+CY_TO_PY_TYPE = {
+    'int' : 'int',
+    'float' : 'float',
+    'double' : 'float',
+    'short' : 'int',
+    'long' : 'int'
+}
+
+FUNC_DEF_STARTS = {
+    'def',
+    'cdef',
+    'cpdef'
+}
